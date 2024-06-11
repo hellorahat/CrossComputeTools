d3ede2b0c44461b01e7c350e69a0584f7bd1822f
hellorahat <rsazad813@gmail.com>
2024-06-11 19:56:38 -0400

    phase 1 iteration 1 complete
    bug fixes, code refactoring, word/phrase suppression

--- DataAnonymizer/Phase1/Iteration1/run.py
+++ DataAnonymizer/Phase1/Iteration1/run.py
@@ -7,7 +7,8 @@ input_folder, output_folder = argv[1:]
 
 def process_string(s, dictionary):
     """
-    This function processes string s data into the dictionary.
+    This function processes string s data into a dictionary.
+    It will be stored as string:chars_to_suppress
     
     Parameters:
     s: The string to be processed.
@@ -53,30 +54,46 @@ class suppressor_class:
         # Process data
         process_string(suppression_columns, self.columns)
         process_string(words_to_suppress, self.words)
-    
+        
     def suppress_data(self, csv_path):
         df = pd.read_csv(csv_path)
+        df = df.astype(str)
+        # Suppress data for columns
         for col_name, chars_to_suppress in self.columns.items():
-            
             if col_name in df.columns:
                 for i, cell in enumerate(df[col_name]):
-                    if(int(chars_to_suppress) == 0):
-                        df.at[i, col_name] = self.suppress_all(cell)
-                    elif(int(chars_to_suppress) > 0):
-                        df.at[i, col_name] = self.suppress_first_chars(cell, int(chars_to_suppress))
-                    elif(int(chars_to_suppress) < 0):
-                        df.at[i, col_name] = self.suppress_last_chars(cell, abs(int(chars_to_suppress)))
+                    df.at[i,col_name] = self.suppress(cell,chars_to_suppress)
                         
+        # Iterate through every cell in the dataframe to locate each word
+        for row_index in range(df.shape[0]):
+            for col_index in range(df.shape[1]):
+                cell_value = df.iat[row_index,col_index]
+                cell_words = cell_value.split()
+                # Suppress if word is found
+                for word_to_suppress, chars_to_suppress in self.words.items():
+                    for i, cell_word in enumerate(cell_words):
+                        if word_to_suppress == cell_word:
+                            cell_words[i] = self.suppress(word_to_suppress, chars_to_suppress)
+                    df.iat[row_index, col_index] = ' '.join(cell_words)
+                
         return df
+    
+    def suppress(self, s, num):
+        if(int(num) == 0):
+            return self._suppress_all(str(s))
+        elif(int(num) > 0):
+            return self._suppress_first_chars(str(s), int(num))
+        elif(int(num) < 0):
+            return self._suppress_last_chars(str(s), abs(int(num)))
                         
-    def suppress_all(self, s):
+    def _suppress_all(self, s):
         result = '*' * len(s)
         return result
     
-    def suppress_first_chars(self, s, num):
+    def _suppress_first_chars(self, s, num):
         return '*' * num + s[num:]
     
-    def suppress_last_chars(self, s, num):
+    def _suppress_last_chars(self, s, num):
         return s[:-num] + '*' * num
                                             
         
