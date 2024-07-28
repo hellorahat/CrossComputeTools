934537f063640dd049fbc5bd5e4f41f1d794cdd9
hellorahat <rsazad813@gmail.com>
2024-07-28 08:44:48 -0400

    added encryption and decryption

---
+++ DataAnonymizer/Phase2/Iteration1/input.md
@@ -0,0 +1,49 @@
+# DataAnonymizer
+#### This tool uses techniques such as suppression and generalization to anonymize large sets of data to protect privacy.
+
+## Step 1: Upload the CSV
+#### Upload your dataset in this step.
+{ csv }
+
+## Step 2: Specify Columns to Suppress
+#### Input the names of the columns here. Each column should be in a separate line. Specify characters to suppress by adding a colon followed by an integer.
+For example - ColumnName:3
+- 0 (or no integer) indicates that all characters will be suppressed
+- An integer n indicates to suppress the first n characters
+- A negative integer n indicates to suppress the last n characters
+
+{ suppression_columns }
+
+## Step 3: Specify Words to Suppress
+#### Separate words in a separate line
+{ words_to_suppress }
+
+## Step 4: Specify Numbers to Round
+#### Input columns where numbers should be rounded. Specify digit to round to by adding a colon followed by an integer.
+For example - Salary:3 - will round the Salary column to the 3rd digit.
+
+{ numbers_to_round }
+
+## Step 5: Specify Locations to Generalize
+#### Input the names of the columns here. Each column should be in a separate line. Specify what region to generalize to by adding a colon followed by region.
+For example:
+- LocationColumn:city
+- LocationColumn:state
+- LocationColumn:country
+
+{ locations_to_generalize }
+
+## Step 6: Specify Columns to Encrypt
+#### Input the names of the columns followed by a colon and the key.
+For example:
+- SecretColumn1:ExamplePassword1234
+- SecretColumn2:Apples324
+
+{ columns_to_encrypt }
+
+## Step 7: Specify Columns to Decrypt
+#### Input the names of the columns followed by a colon and the key.
+- SecretColumn1:ExamplePassword1234
+- SecretColumn2:Apples324
+
+{ columns_to_decrypt }
---
+++ DataAnonymizer/Phase2/Iteration1/run.py
@@ -0,0 +1,324 @@
+import json
+import re
+import math
+import pandas as pd
+from sys import argv
+from os.path import join
+
+from geopy.geocoders import Nominatim
+from geopy.exc import GeocoderTimedOut, GeocoderServiceError
+import pycountry_convert as pc
+
+import base64
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+
+input_folder, output_folder = argv[1:]
+
+class input_processor_class:
+    def __init__(self):
+        self.input_path = join(input_folder, "input.csv")
+        
+        with open(join(input_folder, "variables.dictionary")) as f:
+            data = json.load(f)
+            self.suppression_columns = data["suppression_columns"]
+            self.words_to_suppress = data["words_to_suppress"]
+            self.numbers_to_round = data["numbers_to_round"]
+            self.locations_to_generalize = data["locations_to_generalize"]
+            self.columns_to_encrypt = data["columns_to_encrypt"]
+            self.columns_to_decrypt = data["columns_to_decrypt"]
+            
+class output_processor_class:
+    def __init__(self):
+        self.output_path = join(output_folder, "out.csv")
+    
+class suppressor_class:
+    def __init__(self, suppression_columns, words_to_suppress):
+        # Initialize variables
+        self.columns = {}
+        self.words = {}
+        
+        # Process data
+        self.process_string(suppression_columns, self.columns)
+        self.process_string(words_to_suppress, self.words)
+
+    def process_string(self, s, dictionary):
+        """
+        This function processes string s data into a dictionary.
+        It will be stored as string:chars_to_suppress
+        
+        Parameters:
+        s: The string to be processed.
+        dictionary: The dictionary to process the data into. 
+        """
+        entries = s.split("\n")
+        for entry in entries:
+            parts = entry.split(":")
+            if len(parts) == 2:  # If there are 2 parts, this means that the user specified how many characters should be suppressed
+                col = parts[0]
+                chars_to_suppress = parts[1]
+            else: # otherwise, supress the entire entry
+                col = entry
+                chars_to_suppress = '0'  # '0' indicates to suppress the entire word
+            
+            # # Check to see if the amount of characters to suppress doesn't exceed the length of the word
+            # if(abs(int(chars_to_suppress)) >= len(col)):
+            #     chars_to_suppress = '0' # '0' indicates to suppress the entire word
+
+            dictionary[col] = chars_to_suppress
+        
+    def suppress_data(self, df):
+        # Suppress data for columns
+        for col_name, chars_to_suppress in self.columns.items():
+            if col_name in df.columns:
+                for i, cell in enumerate(df[col_name]):
+                    df.at[i,col_name] = self.suppress(cell,chars_to_suppress)
+                        
+        # Iterate through every cell in the dataframe to locate each word
+        for row_index in range(df.shape[0]):
+            for col_index in range(df.shape[1]):
+                # Suppress if word is found
+                for word_to_suppress, chars_to_suppress in self.words.items():
+                    cell_value = str(df.iat[row_index,col_index])
+                    cell_words = re.split(f"({word_to_suppress})", cell_value)
+                    for i, cell_word in enumerate(cell_words):
+                        if word_to_suppress == cell_word:
+                            cell_words[i] = self.suppress(word_to_suppress, chars_to_suppress)
+                            df.iat[row_index, col_index] = ''.join(cell_words)
+        return df
+    
+    def suppress(self, s, num):
+        if(int(num) == 0):
+            return self._suppress_all(str(s))
+        elif(int(num) > 0):
+            return self._suppress_first_chars(str(s), int(num))
+        elif(int(num) < 0):
+            return self._suppress_last_chars(str(s), abs(int(num)))
+                        
+    def _suppress_all(self, s):
+        result = '*' * len(s)
+        return result
+    
+    def _suppress_first_chars(self, s, num):
+        return '*' * num + s[num:]
+    
+    def _suppress_last_chars(self, s, num):
+        return s[:-num] + '*' * num
+                                            
+        
+class number_rounder:
+    def __init__(self, digits_to_round):
+        # Initialize variables
+        self.columns = {}
+
+        # Process data
+        self.process_string(digits_to_round, self.columns)
+    
+    def process_string(self, s, dictionary):
+        entries = s.split("\n")
+        for entry in entries:
+            parts = entry.split(":")
+            if len(parts) == 2:
+                col = parts[0]
+                digits_to_round = parts[1]
+            else: # if the digit to round to is not specified, round to leftmost digit
+                col = entry
+                digits_to_round = "-1" # -1 means to round the leftmost digit
+
+            dictionary[col] = digits_to_round
+
+    def round_df_cols(self, df):
+        for col_name, digits_to_round in self.columns.items():
+            if col_name in df.columns:
+                for i, cell in enumerate(df[col_name]):
+                    df.at[i,col_name] = self._round(cell,digits_to_round)
+
+    def _round(self, s, digit):
+        num = int(s)
+        digit = int(digit)
+        if num == 0:
+            return 0
+
+        num_abs = abs(num)
+        sign = -1 if num < 0 else 1
+
+        # Calculate the power of 10 for the given digit position
+        power = int(math.log10(num_abs))
+        if digit < 0:
+            power = power + digit + 1
+        else:
+            power = power - digit + 1
+
+        # Calculate the rounding base
+        base = num_abs / (10**power)
+
+        # Perform rounding
+        if base >= 5:
+            base = math.ceil(base)
+        else:
+            base = math.floor(base)
+
+        # Calculate the rounded number
+        rounded_num = base * (10**power)
+
+        return rounded_num * sign
+
+class location_generalizer:
+    def __init__(self, locations_to_generalize):
+        # Initialize variables
+        self.columns = {}
+
+        # Process data
+        self.process_string(locations_to_generalize, self.columns)
+
+    def process_string(self, s, dictionary):
+        entries = s.split("\n")
+        for entry in entries:
+            parts = entry.split(":")
+            if len(parts) == 2:
+                col = parts[0]
+                generalized_location = parts[1]
+            else: # if the digit to round to is not specified, generalize to country
+                col = entry
+                generalized_location = "country"
+
+            dictionary[col] = generalized_location
+
+    def generalize_df_cols(self, df):
+        for col_name, generalized_location in self.columns.items():
+            if col_name in df.columns:
+                for i, cell in enumerate(df[col_name]):
+                    df.at[i,col_name] = self._generalize(cell,generalized_location)
+
+    def _generalize(self, full_location, generalized_location):
+        generalized_location = generalized_location.lower()
+        geolocator = Nominatim(user_agent="location_generalizer")
+        try:
+            location_data = geolocator.geocode(full_location, addressdetails=True)
+            if not location_data:
+                return "Location not found"
+            
+            address = location_data.raw['address']
+            
+            if generalized_location == 'city':
+                return address.get('city', address.get('town', address.get('village', '')))
+            elif generalized_location == 'state':
+                return address.get('state', '')
+            elif generalized_location == 'country':
+                return address.get('country', '')
+            else:
+                return "Invalid level"
+        except (GeocoderTimedOut, GeocoderServiceError) as e:
+            return f"Error: {e}"
+        
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives import hashes
+from cryptography.fernet import Fernet, InvalidToken
+import base64
+
+class encrypter:
+    def __init__(self, columns_to_encrypt):
+        self.columns = {}
+        self.process_string(columns_to_encrypt, self.columns)
+
+    def process_string(self, s, dictionary):
+        entries = s.split("\n")
+        for entry in entries:
+            parts = entry.split(":")
+            if len(parts) == 2:
+                col = parts[0]
+                key = parts[1]
+                key = self.password_to_key(key)
+                dictionary[col] = key
+
+    def encrypt_df(self, df):
+        for col_name, encrypt_key in self.columns.items():
+            if col_name in df.columns:
+                for i, cell in enumerate(df[col_name]):
+                    encrypted_value = self._encrypt(cell, encrypt_key).decode('utf-8')
+                    df.at[i, col_name] = encrypted_value
+
+    def _encrypt(self, s, key):
+        fernet = Fernet(key)
+        return fernet.encrypt(s.encode('utf-8'))
+
+    def password_to_key(self, password):
+        password = password.encode('utf-8')
+        salt = b''  # Use a constant salt for consistency
+        kdf = PBKDF2HMAC(
+            algorithm=hashes.SHA256(),
+            length=32,
+            salt=salt,
+            iterations=480000,
+        )
+        key = base64.urlsafe_b64encode(kdf.derive(password))
+        return key
+
+
+class decrypter:
+    def __init__(self, columns_to_decrypt):
+        self.columns = {}
+        self.process_string(columns_to_decrypt, self.columns)
+
+    def process_string(self, s, dictionary):
+        entries = s.split("\n")
+        for entry in entries:
+            parts = entry.split(":")
+            if len(parts) == 2:
+                col = parts[0]
+                key = parts[1]
+                key = self.password_to_key(key)
+                dictionary[col] = key
+
+    def decrypt_df(self, df):
+        for col_name, decrypt_key in self.columns.items():
+            if col_name in df.columns:
+                for i, cell in enumerate(df[col_name]):
+                    decrypted_value = self._decrypt(cell, decrypt_key)
+                    df.at[i, col_name] = decrypted_value
+
+    def _decrypt(self, s, key):
+        fernet = Fernet(key)
+        try:
+            dec_message = fernet.decrypt(s.encode('utf-8')).decode('utf-8')
+        except:
+            dec_message = "incorrect key"
+        return dec_message
+
+    def password_to_key(self, password):
+        password = password.encode('utf-8')
+        salt = b''  # Use a constant salt for consistency
+        kdf = PBKDF2HMAC(
+            algorithm=hashes.SHA256(),
+            length=32,
+            salt=salt,
+            iterations=480000,
+        )
+        key = base64.urlsafe_b64encode(kdf.derive(password))
+        return key
+
+
+
+if __name__ == "__main__":
+    input_processor = input_processor_class()
+    output_processor = output_processor_class()
+    df = pd.read_csv(input_processor.input_path)
+    df.astype(str)
+    
+    suppressor = suppressor_class(input_processor.suppression_columns, input_processor.words_to_suppress)
+    suppressor.suppress_data(df)
+
+    rounder = number_rounder(input_processor.numbers_to_round)
+    rounder.round_df_cols(df)
+
+    generalizer = location_generalizer(input_processor.locations_to_generalize)
+    generalizer.generalize_df_cols(df)
+
+    encrypter = encrypter(input_processor.columns_to_encrypt)
+    encrypter.encrypt_df(df)
+
+    decrypter = decrypter(input_processor.columns_to_decrypt)
+    decrypter.decrypt_df(df)
+
+    df.to_csv(output_processor.output_path, index=False)
